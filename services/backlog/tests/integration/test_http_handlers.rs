c71b01034569e4d77da210353e093eee58c6325f
masseyis <masseyis@me.com>
2025-09-22 12:37:18 +0100

    test clippy blocking

--- services/backlog/tests/integration/test_http_handlers.rs
+++ services/backlog/tests/integration/test_http_handlers.rs
@@ -5,7 +5,7 @@ use axum::{
 };
 use backlog::create_backlog_router;
 use serde_json::json;
-use sqlx::PgPool;
+use sqlx::PgPool; // Intentional unused import to test clippy blocking
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tower::util::ServiceExt;
